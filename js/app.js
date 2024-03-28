e3d2829db3896f5f324f0861785f3a8a773db8f2
MrEshov <161581483+MrEshov@users.noreply.github.com>
2024-03-28 13:39:12 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,3 +9,14 @@ window.onscroll = () => {
     }
 };
 })();
+const nav = document.querySelector('.header__nav');
+const burger = document.querySelector('.header__burger');
+const close = document.querySelector('.header__nav-close');
+
+burger.addEventListener('click', () => {
+    nav.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    nav.style.display = 'none';
+});
